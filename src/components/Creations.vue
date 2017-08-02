7836e313fee597e8cf78fd559f53b769aaf905ca
enzo_muhlinghaus <enzomuhlinghaus@gmail.com>
2017-08-02 16:53:07 +0200

    Begining to use the Api a bit

--- src/components/Creations.vue
+++ src/components/Creations.vue
@@ -2,13 +2,16 @@
   <div class="creations">
     <h1>Créations</h1>
     <ul>
-      <li v-for="creation in creations">{{ creation.title }}</li>
+      <li v-for="creation in creations">
+        <h3>{{ creation.title }}</h3>
+        <img :src="creation.image" alt="">
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-//  import axios from 'axios'
+  import axios from 'axios'
 
   export default {
     name: 'creations',
@@ -16,17 +19,22 @@
       return {
         creations: []
       }
-    }
+    },
 
-//    mounted: function () {
-//      let self = this
-//      axios.get('url_api').then(function (response) {
-//        self.creations = response.data
-//      })
-//    }
+    mounted: function () {
+      let self = this
+      axios.get('https://nadmcreations.com/api/creations').then(function (response) {
+        self.creations = response.data
+      })
+    }
   }
 </script>
 
 <style scoped>
+
+  ul{
+    list-style: none;
+    padding: 0;
+  }
 
 </style>
