a505b1f5db0bf416af2df8391833a84b6bb6c344
Enzo Muhlinghaus <enzomuhlinghaus@gmail.com>
2017-08-16 01:44:38 +0200

    Transitions

--- src/components/Creations.vue
+++ src/components/Creations.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="creations" v-masonry transition-duration=".3s" item-selector=".item" fit-width="true">
-      <div v-masonry-tile v-for="creation in creations" class="item">
-        <img :src="creation.image" :alt="creation.title" :title="creation.title">
-        <p>{{ creation.description }}</p>
-      </div>
-  </div>
+  <transition name="longFade" appear>
+    <div class="creations" v-masonry transition-duration=".5s" item-selector=".item" fit-width="true">
+        <div v-masonry-tile v-for="creation in creations" class="item">
+          <img :src="creation.image" :alt="creation.title" :title="creation.title">
+          <p>{{ creation.description }}</p>
+        </div>
+    </div>
+  </transition>
 </template>
 
 <script>
